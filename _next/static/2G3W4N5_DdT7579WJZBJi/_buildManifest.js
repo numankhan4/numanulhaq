2489aad79ece2fbecc9961f581f251ddd9b5aa00
numankhan4 <numankhan4@users.noreply.github.com>
2026-06-16 19:00:26 +0000

    deploy: 235f46d9c6c71034195eb94fc9830b39eee9c69a

--- _next/static/2G3W4N5_DdT7579WJZBJi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-29551af6f7b5aa12.js","static/css/025128a968188a94.css","static/chunks/pages/index-2371662bb5ea8af0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/b6VJyGxPNYivHMvSGLclc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-29551af6f7b5aa12.js","static/css/025128a968188a94.css","static/chunks/pages/index-96ef9b1c396b0fe8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
